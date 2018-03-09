841106d6a1dfd8465f40294d79bfe0099b9a5cf9
Rudy De Busscher <rdebusscher@gmail.com>
2018-03-09 21:24:04 +0100

    securedListener custom tag improvement and documentation

--- impl/web/jsf7/src/main/java/be/atbash/ee/security/octopus/view/component/secured/PermissionListenerHandler.java
+++ impl/web/jsf7/src/main/java/be/atbash/ee/security/octopus/view/component/secured/PermissionListenerHandler.java
@@ -15,11 +15,14 @@
  */
 package be.atbash.ee.security.octopus.view.component.secured;
 
+import be.atbash.ee.security.octopus.view.component.OctopusComponentUsageException;
 import be.atbash.util.JsfUtils;
 
+import javax.el.ELException;
 import javax.el.MethodExpression;
 import javax.faces.component.UIComponent;
 import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagAttribute;
 import javax.faces.view.facelets.TagConfig;
 
 /**
@@ -35,8 +38,16 @@ public class PermissionListenerHandler extends OctopusTagHandler {
     void handleSecurity(FaceletContext ctx, UIComponent parent, SecuredComponentData securedComponentData) {
         if (octopusHandlerHelper.hasAccess(securedComponentData)) {
 
-            MethodExpression listener = JsfUtils.createMethodExpression(getAttribute("listener").getValue(), Void.class, UIComponent.class);
-            listener.invoke(ctx.getFacesContext().getELContext(), new Object[]{parent});
+            TagAttribute attribute = getAttribute("listener");
+            if (attribute == null) {
+                throw new OctopusComponentUsageException("The tag securedListener needs a listener attribute that is an EL expression to method with parameter of type UIComponent");
+            }
+            MethodExpression listener = JsfUtils.createMethodExpression(attribute.getValue(), Void.class, UIComponent.class);
+            try {
+                listener.invoke(ctx.getFacesContext().getELContext(), new Object[]{parent});
+            } catch (ELException e) {
+                throw new OctopusComponentUsageException(String.format("The value of listener atribute must be an EL expression to a method with parameter of type UIComponent. Found '%s'", attribute.getValue()));
+            }
         }
 
     }
