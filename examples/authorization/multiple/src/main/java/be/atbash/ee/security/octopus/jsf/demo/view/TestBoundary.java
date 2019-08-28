158c5c56651b20deeb2871e89c8377bb5796bb62
Rudy De Busscher <rdebusscher@gmail.com>
2019-08-28 20:21:29 +0200

    Demo for multiple AuthorizationInfoProvider

---
+++ examples/authorization/multiple/src/main/java/be/atbash/ee/security/octopus/jsf/demo/view/TestBoundary.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014-2019 Rudy De Busscher (https://www.atbash.be)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.atbash.ee.security.octopus.jsf.demo.view;
+
+import be.atbash.ee.security.octopus.authz.annotation.RequiresPermissions;
+import be.atbash.ee.security.octopus.authz.annotation.RequiresUser;
+
+import javax.ejb.Stateless;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ */
+@Stateless
+public class TestBoundary {
+
+    @RequiresPermissions("demo:read:*")
+    public void executeWithPermission() {
+        System.out.println("executeWithPermission demo:read");
+    }
+
+    @RequiresPermissions("special:action:*")
+    public void executeWithSpecialPermission() {
+        System.out.println("executeWithPermission special:action");
+    }
+}
