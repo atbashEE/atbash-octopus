f1c76e56230482c2967463253d218c2fcdd3056b
Rudy De Busscher <rdebusscher@gmail.com>
2019-09-12 20:08:47 +0200

    Fix issues with UnmanagedInstance on some runtimes.

--- impl/web/jsf7/src/main/java/be/atbash/ee/security/octopus/OctopusJSFExtension.java
+++ impl/web/jsf7/src/main/java/be/atbash/ee/security/octopus/OctopusJSFExtension.java
@@ -15,31 +15,44 @@
  */
 package be.atbash.ee.security.octopus;
 
-import be.atbash.ee.security.octopus.config.OctopusJSFConfiguration;
 import be.atbash.ee.security.octopus.view.model.LoginBean;
+import be.atbash.util.StringUtils;
 import org.apache.deltaspike.core.util.bean.BeanBuilder;
 import org.apache.deltaspike.core.util.metadata.builder.DelegatingContextualLifecycle;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.*;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class OctopusJSFExtension implements Extension {
 
-    private OctopusJSFConfiguration config;
-
-    private Class<OctopusJSFConfiguration> configClass = OctopusJSFConfiguration.class;
-
     void configModule(@Observes AfterBeanDiscovery afterBeanDiscovery, BeanManager beanManager) {
-        config = getUnmanagedInstance(beanManager, configClass);
+        String aliasNameLoginBean = getAliasNameLoginBean();
 
-        if (config.getAliasNameLoginbean().length() != 0) {
-            setAlternativeNameForLoginBean(afterBeanDiscovery, beanManager);
+        if (StringUtils.hasText(aliasNameLoginBean)) {
+            setAlternativeNameForLoginBean(afterBeanDiscovery, beanManager, aliasNameLoginBean);
+        }
+    }
+
+    private String getAliasNameLoginBean() {
+        // We are bypassing OctopusJSFConfiguration but there are issues getting the value through Unmanaged Instance.
+        // If needed, We could try to fallback to the Java EE 6 way by getting just a contextual reference.
+        // But the getOptionalValue can't be used since we are still using atbash-config for Java 7 and on a Java 8
+        // version with a real MP config implementation, the getOptionalValue returns Optional<String>
+        // This was solved in the AbstractConfiguration but we are not using this.
+
+        // So this is now very hacky
+        try {
+            return ConfigProvider.getConfig().getValue("aliasNameLoginBean", String.class);
+        } catch (NoSuchElementException e) {
+            return null;
         }
     }
 
 
-    private void setAlternativeNameForLoginBean(AfterBeanDiscovery afterBeanDiscovery, BeanManager beanManager) {
+    private void setAlternativeNameForLoginBean(AfterBeanDiscovery afterBeanDiscovery, BeanManager beanManager, String aliasNameLoginBean) {
         Set<Bean<?>> beans = beanManager.getBeans("loginBean");
 
         AnnotatedType<LoginBean> loginBeanAnnotatedType = beanManager
@@ -51,7 +64,7 @@ public class OctopusJSFExtension implements Extension {
 
             Bean<LoginBean> newBean = new BeanBuilder<LoginBean>(beanManager)
                     .passivationCapable(false).beanClass(LoginBean.class)
-                    .injectionPoints(bean.getInjectionPoints()).name(config.getAliasNameLoginbean())
+                    .injectionPoints(bean.getInjectionPoints()).name(aliasNameLoginBean)
                     .scope(bean.getScope()).qualifiers(bean.getQualifiers())
                     .types(bean.getTypes()).alternative(bean.isAlternative()).nullable(bean.isNullable())
                     .stereotypes(bean.getStereotypes())
@@ -59,12 +72,6 @@ public class OctopusJSFExtension implements Extension {
             afterBeanDiscovery.addBean(newBean);
 
         }
-    }
-
-    private <T> T getUnmanagedInstance(BeanManager beanManager, Class<T> beanClass) {
-        Unmanaged<T> unmanagedConfig = new Unmanaged<T>(beanManager, beanClass);
-        Unmanaged.UnmanagedInstance<? extends T> configInstance = unmanagedConfig.newInstance();
-        return configInstance.produce().inject().postConstruct().get();
     }
 
 }
